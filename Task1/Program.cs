e64b8400f19d5156555bc5a1ac96cb0a08b5c96d
om3r2477-del <om3r2477@gmail.com>
2026-07-01 14:10:59 +0400

    1-read and display user's details

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // read and display user's details
+            Console.Write("please enter your name:");
+            String userName = Console.ReadLine();
+
+            Console.Write("please Enter your age:");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.Write("please Enter your Hight:");
+            double userHight = double.Parse(Console.ReadLine());
+            
+            bool  isStudent = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("name:" + userName);
+            Console.Write("Age:" + userAge);
+            Console.Write("Highe:" + userHight);
+            Console.Write("student:" + isStudent);
+
         }
+
+
+    }
     }
 }
