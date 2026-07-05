1b990a84acd9130ab05ba1b6dcd9717ba8615ded
om3r2477-del <om3r2477@gmail.com>
2026-07-05 15:05:19 +0400

    implementing problem 1 -( Absolute Difference)

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Absolute Difference
+            
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double subtraction = num1 - num2;
+
+            double absoluteDifference = Math.Abs(subtraction);
+
+            Console.WriteLine("The absolute difference is : " + absoluteDifference);
         }
     }
 }
