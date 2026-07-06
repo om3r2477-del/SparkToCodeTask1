f4650b034d936d495c51705955520a38b1437149
om3r2477-del <om3r2477@gmail.com>
2026-07-06 17:07:40 +0400

    implementing problem 2 - (Square Number Function)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,10 +12,31 @@
             string name = Console.ReadLine();
 
             printWelcome(name);
+
+            // task 2 
+            Console.WriteLine("Enter your number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = Square(number);
+            Console.WriteLine("the Square is:" + result);
         }
-       public static void printWelcome(string name)
+
+        
+
+        public static void printWelcome(string name)
         {
             Console.WriteLine("Welcome : " + name);
         }
-    }
+
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        //Task 2 - Square Number Function
+        public static int Square(int number)
+        {
+            return number * number;
+        }
+        
+
+        }
 }
+
