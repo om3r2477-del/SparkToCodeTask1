40e3b71f37525fc313019f0d5fd3a3b66e28d451
om3r2477-del <om3r2477@gmail.com>
2026-07-07 16:13:36 +0400

    implementing problem Task 1 - (Fixed Grades Array)

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+
+            // Task 1 - Fixed Grades Array
+
+            int[]  greads = new int[5];
+
+            for (int i = 0; i < greads.Length; i++) { 
+            
+            Console.Write("Enter gread "+(i + 1) + ":");
+            greads[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("students gread");
+            foreach (int grade in greads)
+                Console.WriteLine(grade);
+        
+    }
     }
 }
