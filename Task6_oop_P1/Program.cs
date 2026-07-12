0f5be16669ec0c66448f5ae85f5d3c818fbb2fa8
om3r2477-del <om3r2477@gmail.com>
2026-07-12 17:19:21 +0400

    add the e six objects

--- Task6_oop_P1/Program.cs
+++ Task6_oop_P1/Program.cs
@@ -24,7 +24,7 @@
                 Console.WriteLine("no enugh balance");
             }
         }
-        public void CheckBalance()
+        public double CheckBalance()
         {
             PrintInformation();
             return Balance;
@@ -72,7 +72,7 @@
 
         public void Sell(int quantity)
         {
-            if(StockQuantity >= quantity)
+            if (StockQuantity >= quantity)
             {
                 StockQuantity -= quantity;
             }
@@ -97,16 +97,50 @@
             Console.WriteLine("Product: " + ProductName);
             Console.WriteLine("Price;" + Price);
             Console.WriteLine("Stock:" + StockQuantity);
-    }
+        }
         private void LogTransaction()
         {
             Console.WriteLine("Transaction is logged");
         }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // BankAccount objects 
+
+
+            BankAccount acount1 = new BankAccount();
+            acount1.AccountNumber = 2002;
+            acount1.HolderName = "Omar";
+            acount1.Balance = 750;
+
+            BankAccount acount2 = new BankAccount();
+            acount2.AccountNumber = 2003;
+            acount2.HolderName = "mutaz";
+            acount2.Balance = 250;
+
+            // // Student objects
+            student student1 = new student();
+            student1.name = "Ali";
+            student1.Address = "Muscat";
+            student1.Grade = 67;
+
+            student student2 = new student();
+            student2.name = "Ahmed";
+            student2.Address = "Muscat";
+            student2.Grade = 80;
+
+            // // Product objects
+            Product prod1 = new Product();
+            prod1.ProductName = "mouse";
+            prod1.Price = 15.5;
+            prod1.StockQuantity = 200;
+
+            Product prod2 = new Product();
+            prod2.ProductName = "laptop";
+            prod2.Price = 121.5;
+            prod2.StockQuantity = 20;
         }
     }
 }
