9e0ff56f66a3328cf797ef371824059ed641f222
om3r2477-del <om3r2477@gmail.com>
2026-07-12 15:19:04 +0400

    implemented 1- (bank Account Class)

--- Task6_oop_P1/Program.cs
+++ Task6_oop_P1/Program.cs
@@ -1,5 +1,46 @@
 ﻿namespace Task6_oop_P1
 {
+    // banckAcoount Class
+    class BankAccount
+    {
+        public int AccountNumber;
+        public string HolderName;
+        public double Balance;
+
+    public void Deposit(double amount)
+        {
+            Balance += amount;
+            sendEmail();
+        }
+     public void Withdraw(double amount)
+        {
+            if(Balance >= amount)
+            {
+                Balance -= amount;
+                sendEmail();
+            }
+            else
+            {
+                Console.WriteLine("no enugh balance");
+            }
+        }
+        public void CheckBalance()
+        {
+            PrintInformation();
+            return Balance;
+        }
+        private void PrintInformation()
+        {
+            Console.WriteLine("name" + HolderName);
+            Console.WriteLine("Balance" + Balance);
+        }
+    private void SendEmail()
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+
+    //
     internal class Program
     {
         static void Main(string[] args)
