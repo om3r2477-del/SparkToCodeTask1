adaf630841a1dbb1987eee3e08f41fa290c75de8
om3r2477-del <om3r2477@gmail.com>
2026-07-12 15:03:00 +0400

    add project files

---
+++ Task6_oop_P1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task6_oop_P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
