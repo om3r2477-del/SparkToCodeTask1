230a960f42362d1d4b9fdd22b66429145f4c2b14
om3r2477-del <om3r2477@gmail.com>
2026-07-24 16:28:44 +0400

    add clases

---
+++ EFCoreProject/Models/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreProject.Models
+{
+    public class BankAccount
+    {
+        public int ID { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public string Currency { get; set; }
+        public DateTime OpenDate { get; set; }
+        public string Status { get; set; }
+        public int BankID { get; set; }
+    }
+}
---
+++ EFCoreProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
