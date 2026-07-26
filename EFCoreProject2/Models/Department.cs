86e7346cc8ce10330809874f127bb87cc2a342ca
om3r2477-del <om3r2477@gmail.com>
2026-07-26 14:36:57 +0400

    Add clases (Emloyee,Department)

---
+++ EFCoreProject2/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreProject2.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; } 
+
+        public int DepartmentNumber { get; set; } 
+
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ EFCoreProject2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreProject2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
