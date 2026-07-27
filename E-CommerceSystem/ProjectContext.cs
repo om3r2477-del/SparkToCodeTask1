a0098dbc2fe713d37c97a70896a374d9b15c43d6
om3r2477-del <om3r2477@gmail.com>
2026-07-27 18:18:11 +0400

    add Project Context

--- E-CommerceSystem/ProjectContext.cs
+++ E-CommerceSystem/ProjectContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E_CommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,24 @@ namespace E_CommerceSystem
 {
     public class ProjectContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=DESKTOP-QENHPGP\\SQLEXPRESS;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
     }
 }
