08f6143a22d8679eda15e73619a7e8085da66834
om3r2477-del <om3r2477@gmail.com>
2026-07-27 18:12:30 +0400

    implement class (order)

--- E-CommerceSystem/Models/Order.cs
+++ E-CommerceSystem/Models/Order.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystem.Models
 {
     public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        // User 1 : M Order (places)
+        [ForeignKey("user")]
+        public int UserId { get; set; }
+
+        public User user { get; set; }
+
+        // Order 1 : 1 Review (has)
+        public Review Review { get; set; }
+
+        // Order M : N Product (contains)
+        public List<OrderProduct> OrderProducts { get; set; }
     }
 }
