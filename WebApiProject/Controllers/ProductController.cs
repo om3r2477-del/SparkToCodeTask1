e240eb3abc88e2e87ac9ce428c83f045577a0cef
om3r2477-del <om3r2477@gmail.com>
2026-07-29 16:15:34 +0400

    updateProduct

--- WebApiProject/Controllers/ProductController.cs
+++ WebApiProject/Controllers/ProductController.cs
@@ -41,6 +41,66 @@ namespace WebApiProject.Controllers
                 context.SaveChanges();
                 return Ok("removed successfully");
             }
+
         }
+        [HttpPatch("UpdateProductPrice")]
+        public IActionResult UpdateProductPrice(int id, double newPrice)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            p.ProductPrice = newPrice;
+
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPatch("UpdateProductName")]
+        public IActionResult UpdateProductName(int id, string newName)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            p.ProductName = newName;
+
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpPut("UpdateProduct")]
+        public IActionResult UpdateProduct(int id, Product newProduct)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            p.ProductPrice = newProduct.ProductPrice;
+            p.ProductName = newProduct.ProductName;
+            p.ProductDescription = newProduct.ProductDescription;
+
+            context.SaveChanges();
+
+            return Ok();
+        }
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+            return Ok(p);
+        }
+
+        [HttpGet("GetALLProducts")]
+        public IActionResult GetALLProducts()
+        {
+            List<Product> products = context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            List<Product> products = context.Products.Where(p => p.ProductName.Contains(name)).ToList();
+            return Ok(products);
+        }
+
     }
 }
