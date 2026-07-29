0a34068da478c83434baf4bef1448f7e9f2457b4
om3r2477-del <om3r2477@gmail.com>
2026-07-29 16:08:10 +0400

    RemoveProduct

--- WebApiProject/Controllers/ProductController.cs
+++ WebApiProject/Controllers/ProductController.cs
@@ -25,6 +25,22 @@ namespace WebApiProject.Controllers
 
             return Ok(p.ProductId);
         }
+        [HttpDelete("RemoveProduct")]
+        public IActionResult RemoveProduct(int id)
+        {
 
+            Product p = context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (p == null)
+            {
+                return NotFound("product not found");
+            }
+            else
+            {
+                context.Products.Remove(p);
+                context.SaveChanges();
+                return Ok("removed successfully");
+            }
+        }
     }
 }
